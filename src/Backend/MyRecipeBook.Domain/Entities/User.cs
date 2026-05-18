932cc6c7b5e35df519320e211e74b457dec4f1a6
João Victor <123279954+joaodev2005@users.noreply.github.com>
2026-05-18 19:18:29 -0300

    feat: add mapping entities to user entity

--- src/Backend/MyRecipeBook.Domain/Entities/User.cs
+++ src/Backend/MyRecipeBook.Domain/Entities/User.cs
@@ -4,7 +4,6 @@ public class User
 {
     public Guid Id { get; private set; } = Guid.CreateVersion7();
     public bool Active { get; set; } = true;
-
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
