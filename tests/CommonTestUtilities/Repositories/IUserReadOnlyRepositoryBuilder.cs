7aa768e851af9c848b68e5216ea811077627f227
João Victor <123279954+joaodev2005@users.noreply.github.com>
2026-07-03 16:48:51 -0300

    tests: unit test use case login

--- tests/CommonTestUtilities/Repositories/IUserReadOnlyRepositoryBuilder.cs
+++ tests/CommonTestUtilities/Repositories/IUserReadOnlyRepositoryBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using Moq;
+using MyRecipeBook.Domain.Entities;
 using MyRecipeBook.Domain.Repositories.User;
 
 namespace CommonTestUtilities.Repositories;
@@ -16,6 +17,12 @@ public class IUserReadOnlyRepositoryBuilder
     {
         _mock.Setup(repository => repository.ExistActiveUserWithEmail(email))
             .ReturnsAsync(true);
+    }
+
+    public void GetByEmail(User user)
+    {
+        _mock.Setup(repository => repository.GetByEmail(user.Email))
+            .ReturnsAsync(user);
     }
 
     public IUserReadOnlyRepository Build() => _mock.Object;
