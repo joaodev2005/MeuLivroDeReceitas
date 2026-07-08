45350e66950f228a30aba4294b7ea34a841df376
João Victor <123279954+joaodev2005@users.noreply.github.com>
2026-07-08 11:30:32 -0300

    test: unit and integration test for logged user and user profile

--- tests/WebApi.Tests/MyRecipeBookApplicationFactory.cs
+++ tests/WebApi.Tests/MyRecipeBookApplicationFactory.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MyRecipeBook.Domain.Security.PasswordHashing;
+using MyRecipeBook.Domain.Security.Tokens;
 using MyRecipeBook.Infrastructure.DataAccess;
 using Testcontainers.MySql;
 using WebApi.Tests.Resources;
@@ -12,7 +13,7 @@ namespace WebApi.Tests;
 
 public class MyRecipeBookApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
 {
-    public UserIdentifyManager User1 { get; private set; }
+    public UserIdentifyManager User1 { get; private set; } = default!;
 
     private readonly MySqlContainer _mySqlContainer;
 
@@ -44,6 +45,7 @@ public class MyRecipeBookApplicationFactory : WebApplicationFactory<Program>, IA
 
         var dbContext = scope.ServiceProvider.GetRequiredService<MyRecipeBookDbContext>();
         var passwordHasher = scope.ServiceProvider.GetRequiredService<IPasswordHasher>();
+        var accessTokenGenerator = scope.ServiceProvider.GetRequiredService<IAccessTokenGenerator>();
 
         var (user, password) = UserBuilder.Build();
 
@@ -52,7 +54,9 @@ public class MyRecipeBookApplicationFactory : WebApplicationFactory<Program>, IA
         await dbContext.Users.AddAsync(user);
         await dbContext.SaveChangesAsync();
 
-        User1 = new UserIdentifyManager(user, password);
+        var user1AccessToken = accessTokenGenerator.Generate(user);
+
+        User1 = new UserIdentifyManager(user, password, user1AccessToken);
     }
 
     Task IAsyncLifetime.DisposeAsync() => _mySqlContainer.StopAsync();
